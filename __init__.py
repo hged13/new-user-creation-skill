523fc80cd2d9eb0a6dca82348ea3309ed5d3361a
Hannah Gedlaman <91993290+hged13@users.noreply.github.com>
2022-12-01 07:30:09 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,40 +19,8 @@ class NewUserCreation(MycroftSkill):
         artist = self.get_response("Who is your favorite artist?")
         self.speak_dialog("now we will take 5, 7 second samples of your voice")
         i = 0
+        return i
      
-   
-    def start_recording(self, name, num)
-       filename = name + str(num) + ".wav"
-        frames = 1024
-        FORMAT = pyaudio.paInt16
-        channels = 1
-        sample_rate = 22050
-        record_seconds = 7
-        p = pyaudio.PyAudio()
-        stream = p.open(format=FORMAT,
-                    channels=channels,
-                    rate=sample_rate,
-                    input=True,
-                    output=True,
-                    frames_per_buffer=frames)
-        frames2 = []
-        self.speak_dialog("Recording...")
-            for i in range(int(44100 / frames * record_seconds)):
-            data = stream.read(frames)
-
-            frames2.append(data)
-        self.speak_dialog("Finished recording.")
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
-        wf = wave.open(filename, "wb")
-        wf.setnchannels(channels)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(sample_rate)
-        wf.writeframes(b"".join(frames2))
-        wf.close()
-        return filename
-
 
 def create_skill():
     return NewUserCreation()
