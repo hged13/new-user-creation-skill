50db255a85cddb219f6b409e3fd1c2d679a817a1
Hannah Gedlaman <91993290+hged13@users.noreply.github.com>
2022-11-30 17:33:34 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,8 +7,12 @@ class NewUserCreation(MycroftSkill):
 
     @intent_file_handler('creation.user.new.intent')
     def handle_creation_user_new(self, message):
-        test = 0
+        self.test_function()
         self.speak_dialog('creation.user.new')
+        
+    def test_function(self):
+        test = 0
+        return test
 
 
 def create_skill():
