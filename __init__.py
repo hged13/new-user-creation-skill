849ad04ee67810e33cd807573b9bcf0472523236
Hannah Gedlaman <91993290+hged13@users.noreply.github.com>
2022-11-30 18:06:31 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import csv 
+import pyaudio
+import wave
+import pandas as pd
 
 class NewUserCreation(MycroftSkill):
     def __init__(self):
@@ -7,12 +10,30 @@ class NewUserCreation(MycroftSkill):
 
     @intent_file_handler('creation.user.new.intent')
     def handle_creation_user_new(self, message):
-        name = self.create_user()
+        info = self.create_user()
         self.speak_dialog('creation.user.new')
     
     def create_user(self):
         name = self.get_response("What is your name")
-        return name
+        playlist = self.get_response("what is your go-to playlist")
+        artist = self.get_response("Who is your favorite artist?")
+        self.speak_dialog("now we will take 5, 7 second samples of your voice")
+        i = 0
+        recordings = []
+       
+
+        while i < 5:
+            file = start_recording(name, i)
+            recordings.append(file)
+            i += 1
+
+        user_info = [
+            name, playlist, artist, recordings]
+            
+        return user_info
+        
+
+       
         
     def test_function(self):
         test = 0
