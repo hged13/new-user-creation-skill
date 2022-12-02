5411486381cf595778560ad4596a5eb17f37a862
Hannah Gedlaman <91993290+hged13@users.noreply.github.com>
2022-12-01 18:36:58 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,4 @@
-from mycroft import MycroftSkill, intent_file_handler
-import csv 
-import pyaudio
-import wave
-import pandas as pd
-
-class NewUserCreation(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
-        self.log.info(self.file_system.path)
-        
-    @intent_file_handler('creation.user.new.intent')
-    def handle_creation_user_new(self, message):
+ def handle_creation_user_new(self, message):
         line = self.create_user() 
         with self.file_system.open('log.csv', "a") as my_file:
             writer = csv.writer(my_file)
@@ -30,13 +18,15 @@ class NewUserCreation(MycroftSkill):
         self.speak_dialog("now we will take 5, 7 second samples of your voice")
         i = 0
         rec = self.start_recording(name, 1)
-        return response
-    
-    def start_recording(self,name, num):
-        filename = name + str(num) + ".wav"
-        frames = 1024
-        FORMAT = pyaudio.paInt16
-        channels = 1
+         return response
+
+     def start_recording(self,name, num):
+         dir = self.file_system.path
+         namelite = name + str(num) + ".wav"
+         filename = dir + "/" + namelite
+         frames = 1024
+         FORMAT = pyaudio.paInt16
+         channels = 1
         sample_rate = 22050
         record_seconds = 7
         p = pyaudio.PyAudio()
@@ -52,13 +42,16 @@ class NewUserCreation(MycroftSkill):
             data = stream.read(frames)
             frames2.append(data)
         print("Finished recording.")
-         stream.stop_stream()
-         stream.close()
-         p.terminate()
-         wf = self.file_system.wave.open(filename, "wb")
-         wf.setnchannels(channels)
-         wf.setsampwidth(p.get_sample_size(FORMAT))
-         wf.setframerate(sample_rate)
-         wf.writeframes(b"".join(frames2))
-         wf.close()
-         return filename
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        wf = wave.open(filename, "wb")
+        wf.setnchannels(channels)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setframerate(sample_rate)
+        wf.writeframes(b"".join(frames2))
+        wf.close()
+        return filename
+     
+def create_skill():
+    return NewUserCreation()
